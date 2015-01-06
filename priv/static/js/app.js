6ffdd36a649cf41ff9c8ba3d6593c147a1611e81
Chris Sphinx <chrissphinx@gmail.com>
2015-01-06 04:51:10 -0500

    Update README.md
    Also fixed sticky footer and scrolling behavior with dynamic resizing.

--- priv/static/js/app.js
+++ priv/static/js/app.js
@@ -1,9 +1,17 @@
 $(function(){
+
   var socket     = new Phoenix.Socket("ws://" + location.host +  "/ws");
   var $status    = $("#status");
   var $messages  = $("#messages");
   var $input     = $("#message-input");
   var $username  = $("#username");
+  var $body      = $("body");
+
+  $(window).resize(function() {
+    $messages.height(($body.height() - 120) + "px");
+    $messages.scrollTop($messages[0].scrollHeight);
+  }).resize();
+
   var sanitize   = function(html){ return $("<div/>").text(html).html(); }
 
   var messageTemplate = function(message){
@@ -27,12 +35,13 @@ $(function(){
 
     chan.on("new:msg", function(message){
       $messages.append(messageTemplate(message));
-      scrollTo(0, document.body.scrollHeight);
+      $messages.scrollTop($messages[0].scrollHeight);
     });
 
     chan.on("user:entered", function(msg){
       var username = sanitize(msg.user || "anonymous");
       $messages.append("<br/><i>[" + username + " entered]</i>");
+      $messages.scrollTop($messages[0].scrollHeight);
     });
   });
 });
