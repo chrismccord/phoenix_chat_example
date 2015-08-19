895067a08a4a64def862e6af5e5d5e43edd99b09
Justin Schneck <jschneck@mac.com>
2015-08-19 14:34:58 -0400

    updated to Phoenix 0.17

--- mix.exs
+++ mix.exs
@@ -22,9 +22,9 @@ defmodule Chat.Mixfile do
   #
   # Type `mix help deps` for examples and options
   defp deps do
-    [{:phoenix, "~> 0.16"},
+    [{:phoenix, "~> 0.17"},
      {:phoenix_html, "~> 2.1"},
-     {:phoenix_live_reload, "~> 0.6"},
+     {:phoenix_live_reload, "~> 1.0"},
      {:phoenix_ecto, "~> 0.9"},
      {:postgrex, ">= 0.0.0"},
      {:cowboy, "~> 1.0"}]
