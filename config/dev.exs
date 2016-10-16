bbfa6f9201f04a7b84c448cf7071682d8fe09ac7
Dr.Thunder <thunderheavyindustries@gmail.com>
2016-10-16 01:37:51 -0600

    chore: point db to localhost

--- config/dev.exs
+++ config/dev.exs
@@ -31,4 +31,6 @@ config :chat, Chat.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
   password: "postgres",
-  database: "chat_dev"
+  database: "chat_dev",
+  hostname: "localhost",
+  pool_size: 10
